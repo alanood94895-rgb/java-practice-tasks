b0b129e36ae94f1dba17f75a7c44302340c50822
CompanyProfile <alanood94895@gmail.com>
2026-04-07 12:22:53 +0400

    task9:maxMod5

---
+++ src/maxMod5.java
@@ -0,0 +1,15 @@
+public class maxMod5 {
+    public static void main(String[] args) {
+        System.out.println(maxMod5(2, 3));
+
+    }
+    public static int maxMod5(int a, int b) {
+        if (a == b) {
+            return 0;
+        }
+        if (a % 5 == b % 5) {
+            return Math.min(a, b);
+        }
+        return Math.max(a, b);
+    }
+}
