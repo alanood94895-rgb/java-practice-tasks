2462eb7d9d23c26ed647f860f8a2355bbb42672b
CompanyProfile <alanood94895@gmail.com>
2026-04-07 12:50:55 +0400

    task12:sotaSum

--- src/sortaSum.java
+++ src/sortaSum.java
@@ -1,13 +1,14 @@
-public class in1To10 {
+public class sortaSum {
     public static void main(String[] args) {
         System.out.println(sortaSum(3, 4));
     }
     public static int sortaSum(int a, int b) {
         int sum = a + b;
+
         if (sum >= 10 && sum <= 19) {
             return 20;
         }
+
         return sum;
     }
 }
-
