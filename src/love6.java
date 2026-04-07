8f5a605060002e45fe99c9ad3161c6051a40336a
CompanyProfile <alanood94895@gmail.com>
2026-04-07 11:27:22 +0400

    task3:love6

---
+++ src/love6.java
@@ -0,0 +1,25 @@
+public class love6 {
+    public static void main(String[] args) {
+        System.out.println(love6(6, 4));
+
+    }
+
+    public static boolean love6(int a, int b) {
+
+        if (a == 6 || b == 6) {
+            return true;
+        }
+
+        if (a + b == 6) {
+            return true;
+        }
+
+        if (Math.abs(a - b) == 6) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
