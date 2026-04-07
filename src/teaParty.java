165ff1c5c43fe2be7b4a3af98c43fcc92548e463
CompanyProfile <alanood94895@gmail.com>
2026-04-07 11:41:48 +0400

    task6:teaParty

--- src/teaParty.java
+++ src/teaParty.java
@@ -1,6 +1,15 @@
 public class teaParty {
     public static void main (String[] args){
-        System.out.println(teaParty ());
+        System.out.println(teaParty (3,8));
+    }
+    public static int teaParty(int tea, int candy){
+        if (tea < 5|| candy < 5 ){
+            return 0;
+        }
+        if (tea >= 2 * candy || candy >= 2 * tea) {
+            return 2;
+        }
+        return 1;
     }
 
 }
