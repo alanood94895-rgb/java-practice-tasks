35b6b5949c7753927e936dbc8834d8058ebb2e53
CompanyProfile <alanood94895@gmail.com>
2026-04-07 12:54:12 +0400

    task13:in1To10

---
+++ src/in1To10.java
@@ -0,0 +1,14 @@
+public class in1To10 {
+    public static void main(String[] args) {
+        System.out.println(in1To10(5, false));
+    }
+    public static boolean in1To10(int n, boolean outsideMode) {
+        if (outsideMode) {
+
+            return (n <= 1 || n >= 10);
+        } else {
+
+            return (n >= 1 && n <= 10);
+        }
+    }
+}
