78098d7e689a5cf5064ab997d618fe5773fecc28
CompanyProfile <alanood94895@gmail.com>
2026-04-07 10:38:17 +0400

    task1:CigarParty

---
+++ src/CigarParty.java
@@ -0,0 +1,20 @@
+public class CigarParty {
+    public static void main(String[] args) {
+        System.out.println(cigarParty(70, false));
+    }
+
+    public static boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            if (cigars >= 40) {
+                return true;
+            }
+            return false;
+        } else {
+            if (cigars >= 40 && cigars <= 60) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
