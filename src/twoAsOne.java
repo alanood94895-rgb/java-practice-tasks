974d1590aef7ccdf4805e644ed6ea451e015fca4
CompanyProfile <alanood94895@gmail.com>
2026-04-07 12:12:38 +0400

    task7:twoAsOne

---
+++ src/twoAsOne.java
@@ -0,0 +1,20 @@
+public class twoAsOne {
+    public static void main(String[] args) {
+        System.out.println(twoAsOne(1, 2, 3));
+
+    }
+
+    public static boolean twoAsOne(int a, int b, int c) {
+
+        if (a + b == c) {
+            return true;
+        }
+        if (a + c == b) {
+            return true;
+        }
+        if (b + c == a) {
+            return true;
+        }
+        return false;
+    }
+}
